d6d354c0a9891012d7a49b17db96df340c30dbe6
chasedputnam <chase.putnam@appian.com>
2017-07-18 15:31:15 +1000

    Initial Project Files
    Adding initial project files to repo.

---
+++ WpfFormLibrary/ViewModel/ComponentVersionInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfFormLibrary.ViewModel
+{
+    public class ComponentVersionInfo
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+    }
+}
